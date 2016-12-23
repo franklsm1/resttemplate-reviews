5c4338c1775156a578af47d7120f4b9f0cb87d02
Jeff Dean <jeff@galvanize.com>
2016-12-22 21:04:26 -0700

    Added integration tests for first endpoints challenge

--- 01-endpoints/integration/java/com/example/EndpointsTest.java
+++ 01-endpoints/integration/java/com/example/EndpointsTest.java
@@ -5,15 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,18 +21,27 @@ public class EndpointsTest {
     private TestRestTemplate template;
 
     @Test
-    public void testCreate() throws Exception {
-        //
-        // ResponseEntity<Person> createPersonResponse = template.postForEntity("/people", person, Person.class);
-        //
-        // ResponseEntity<List<Person>> response = template.exchange(
-        //         "/people",
-        //         HttpMethod.GET,
-        //         null,
-        //         new ParameterizedTypeReference<List<Person>>() {
-        //         }
-        // );
-        //
-        // assertThat(response.getBody().get(0).getId(), equalTo(createPersonResponse.getBody().getId()));
+    public void it_returns_correctly_when_both_parameters_are_present() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/drivers?status=inactive&rating=4", String.class);
+
+        assertThat(response.getBody(), containsString("Looking for inactive drivers with a rating of 4"));
+
+        ResponseEntity<String> response2 = template.getForEntity("/drivers?status=paused&rating=3", String.class);
+
+        assertThat(response2.getBody(), containsString("Looking for paused drivers with a rating of 3"));
+    }
+
+    @Test
+    public void it_defaults_status_to_active() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/drivers?rating=12", String.class);
+
+        assertThat(response.getBody(), containsString("Looking for active drivers with a rating of 12"));
+    }
+
+    @Test
+    public void it_errors_out_if_rating_is_missing() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/drivers", String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
     }
 }
