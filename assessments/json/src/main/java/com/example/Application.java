7939a720b87e20636da80e707da9a032f8a3c4fa
Jeff Dean <jeff@galvanize.com>
2017-03-17 01:03:30 -0600

    updated json assessment

--- assessments/json/src/main/java/com/example/Application.java
+++ assessments/json/src/main/java/com/example/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrollRideApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrollRideApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
